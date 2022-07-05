5846c595892197377c214a48624d58d4f4cdf3fb
Andrea Rosentel <andrearosentel@gmail.com>
2022-07-05 07:55:07 -0700

    createNewToy rough draft

--- src/index.js
+++ src/index.js
@@ -21,25 +21,56 @@ document.addEventListener("DOMContentLoaded", function() {
 function fetchToys() {
   return fetch('http://localhost:3000/toys')
   .then(resp => resp.json())
-  .then((json) => createNewToy(json))
+  .then((json) => createToyCard(json))
 }
 
-function createNewToy(toys) {
-  const newToy = document.getElementById("toy-collection")
+function createToyCard(toys) {
+  const toyList = document.getElementById("toy-collection")
   toys.forEach(toy => {
-    const divCard = document.createElement("div")
-    divCard.className = "card"
+    const toyCard = document.createElement("div")
+    toyCard.className = "card"
     const h2 = document.createElement('h2')
     h2.innerHTML = toy.name
     const img = document.createElement('img')
     img.src = `${toy.image}`
     img.className = "toy-avatar"
     const p = document.createElement('p')
-    p.innerHTML = `${toy.likes}`
+    p.innerHTML = `${toy.likes} likes`
     const btn = document.createElement('button')
-
-    newToy.append(divCard)
-    divCard.appendChild(h2, img, p, btn)
+    btn.className = "like-btn"
+    btn.id = `${toy.id}`
+    btn.innerHTML = "like &#10084",
+    toyList.append(toyCard)
+    toyCard.appendChild(h2)
+    toyCard.appendChild(img)
+    toyCard.appendChild(p)
+    toyCard.appendChild(btn)
 
   })
+}
+const form = document.querySelector("form")
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  createNewToy()
+})
+
+function createNewToy() {
+  
+  fetch('http://localhost:3000/toys', {
+    method: "POST",
+    headers:
+      {
+        "Content-Type": "application/json",
+          Accept: "application/json"
+      },
+
+    body: JSON.stringify({
+      "name": `${input.name.value}`,
+      "image": `${input.image.value}`,
+      "likes": 0
+      })
+      })
+    .then(resp => resp.json())
+    .then((json) => createToyCard())
 }
